4434f3f99c33e9465f9e5a4205d9774177df11eb
Yosuke Nakano <ynakano@moff.mobi>
2019-04-26 17:00:19 +0900

    add script

---
+++ docfxSample/Assets/Scripts/HogeComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Hoge component.
+/// </summary>
+public class HogeComponent : MonoBehaviour {
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start() {
+
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update() {
+
+    }
+}
