759ac21af9cfab78188eeb357bd8dd75c293955c
Yosuke Nakano <ynakano@moff.mobi>
2019-04-26 19:37:32 +0900

    add property

--- unity/Assets/Scripts/HogeComponent.cs
+++ unity/Assets/Scripts/HogeComponent.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyPlugin;
 
 namespace docfxSample {
     /// <summary>
     /// Hoge component.
     /// </summary>
     public class HogeComponent : MonoBehaviour {
+        /// <summary>
+        /// The fuga data.
+        /// </summary>
+        FugaData fugaData;
+        /// <summary>
+        /// Gets the type of the piyo.
+        /// </summary>
+        /// <value>The type of the piyo.</value>
+        public PiyoType PiyoType { private set; get; }
 
+        public void Init(FugaData fuga, PiyoType piyo) {
+            fugaData = fuga;
+            PiyoType = piyo;
+        }
         /// <summary>
         /// Start is called before the first frame update
         /// </summary>
