dd09c97d799b3bd8a1bb953f133974be75e6a682
Yosuke Nakano <ynakano@moff.mobi>
2019-04-26 17:46:31 +0900

    name space

--- docfxSample/Assets/Scripts/HogeComponent.cs
+++ docfxSample/Assets/Scripts/HogeComponent.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Hoge component.
-/// </summary>
-public class HogeComponent : MonoBehaviour {
-
+namespace docfxSample {
     /// <summary>
-    /// Start is called before the first frame update
+    /// Hoge component.
     /// </summary>
-    void Start() {
+    public class HogeComponent : MonoBehaviour {
 
-    }
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        void Start() {
 
-    /// <summary>
-    /// Update is called once per frame
-    /// </summary>
-    void Update() {
+        }
 
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        void Update() {
+
+        }
     }
 }
