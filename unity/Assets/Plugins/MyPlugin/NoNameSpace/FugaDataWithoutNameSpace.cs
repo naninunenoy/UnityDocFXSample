b62f4642ff4d64eb0bceb2d853d4bc51db592c15
Yosuke Nakano <ynakano@moff.mobi>
2019-05-08 16:52:01 +0900

    change class name

---
+++ unity/Assets/Plugins/MyPlugin/NoNameSpace/FugaDataWithoutNameSpace.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// namespaceを定義しないとdocFXには無視される
+/// </summary>
+public class FugaFugaDataWithoutNameSpace {
+    public int Number { set; get; }
+    public string Message { set; get; }
+}
